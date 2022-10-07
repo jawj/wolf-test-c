22ac4dce83bcc3f6dad9bd96065a1ab3ac58b7ee
George MacKerron <george@mackerron.co.uk>
2022-10-07 16:31:23 +0100

    Now working

--- c/tls-test.c
+++ c/tls-test.c
@@ -27,11 +27,14 @@ This is a simple test program to retrieve files over HTTP/1.0 + TLS
 # download from GitHub releases + untar
 cd wolfssl-5.5.1-stable
 brew install brew install autoconf automake libtool
-./autogen
+
+./autogen.sh
+
 ./configure \
-  --disable-filesystem --disable-examples --disable-oldtls \
-  --enable-sni --enable-tls13 \
-  --enable-altcertchains  # alt cert chains are required for Let's Encrypt certs: https://github.com/wolfSSL/wolfssl/issues/4443
+  --disable-filesystem --disable-examples \
+  --enable-static --enable-tls13 --disable-oldtls --enable-sni \
+  --enable-altcertchains --disable-asm --enable-fastmath
+
 make
 make install  # to /usr/local/include
 
@@ -43,15 +46,15 @@ make tls-test
 
 */
 
-#include <stdlib.h>
-#include <stdio.h>
-#include <string.h>
 #include <errno.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 /* socket includes */
-#include <sys/socket.h>
 #include <arpa/inet.h>
 #include <netinet/in.h>
+#include <sys/socket.h>
 #include <unistd.h>
 
 /* wolfSSL */
@@ -60,246 +63,242 @@ make tls-test
 
 #include "../certs/cacert.c"
 
-int my_IORecv(WOLFSSL* ssl, char* buff, int sz, void* ctx)
-{
-    /* By default, ctx will be a pointer to the file descriptor to read from.
-     * This can be changed by calling wolfSSL_SetIOReadCtx(). */
-    int sockfd = *(int*)ctx;
-    int recvd;
+int my_IORecv(WOLFSSL* ssl, char* buff, int sz, void* ctx) {
+  /* By default, ctx will be a pointer to the file descriptor to read from.
+   * This can be changed by calling wolfSSL_SetIOReadCtx(). */
+  int sockfd = *(int*)ctx;
+  int recvd;
 
-    /* Receive message from socket */
-    if ((recvd = recv(sockfd, buff, sz, 0)) == -1) {
-        /* error encountered. Be responsible and report it in wolfSSL terms */
+  /* Receive message from socket */
+  if ((recvd = recv(sockfd, buff, sz, 0)) == -1) {
+    /* error encountered. Be responsible and report it in wolfSSL terms */
 
-        fprintf(stderr, "IO RECEIVE ERROR: ");
-        switch (errno) {
-        #if EAGAIN != EWOULDBLOCK
-        case EAGAIN: /* EAGAIN == EWOULDBLOCK on some systems, but not others */
-        #endif
-        case EWOULDBLOCK:
-            if (!wolfSSL_dtls(ssl) || wolfSSL_get_using_nonblock(ssl)) {
-                fprintf(stderr, "would block\n");
-                return WOLFSSL_CBIO_ERR_WANT_READ;
-            }
-            else {
-                fprintf(stderr, "socket timeout\n");
-                return WOLFSSL_CBIO_ERR_TIMEOUT;
-            }
-        case ECONNRESET:
-            fprintf(stderr, "connection reset\n");
-            return WOLFSSL_CBIO_ERR_CONN_RST;
-        case EINTR:
-            fprintf(stderr, "socket interrupted\n");
-            return WOLFSSL_CBIO_ERR_ISR;
-        case ECONNREFUSED:
-            fprintf(stderr, "connection refused\n");
-            return WOLFSSL_CBIO_ERR_WANT_READ;
-        case ECONNABORTED:
-            fprintf(stderr, "connection aborted\n");
-            return WOLFSSL_CBIO_ERR_CONN_CLOSE;
-        default:
-            fprintf(stderr, "general error\n");
-            return WOLFSSL_CBIO_ERR_GENERAL;
+    fprintf(stderr, "IO RECEIVE ERROR: ");
+    switch (errno) {
+#if EAGAIN != EWOULDBLOCK
+      case EAGAIN: /* EAGAIN == EWOULDBLOCK on some systems, but not others */
+#endif
+      case EWOULDBLOCK:
+        if (!wolfSSL_dtls(ssl) || wolfSSL_get_using_nonblock(ssl)) {
+          fprintf(stderr, "would block\n");
+          return WOLFSSL_CBIO_ERR_WANT_READ;
+        } else {
+          fprintf(stderr, "socket timeout\n");
+          return WOLFSSL_CBIO_ERR_TIMEOUT;
         }
-    }
-    else if (recvd == 0) {
-        puts("Connection closed");
+      case ECONNRESET:
+        fprintf(stderr, "connection reset\n");
+        return WOLFSSL_CBIO_ERR_CONN_RST;
+      case EINTR:
+        fprintf(stderr, "socket interrupted\n");
+        return WOLFSSL_CBIO_ERR_ISR;
+      case ECONNREFUSED:
+        fprintf(stderr, "connection refused\n");
+        return WOLFSSL_CBIO_ERR_WANT_READ;
+      case ECONNABORTED:
+        fprintf(stderr, "connection aborted\n");
         return WOLFSSL_CBIO_ERR_CONN_CLOSE;
+      default:
+        fprintf(stderr, "general error\n");
+        return WOLFSSL_CBIO_ERR_GENERAL;
     }
+  } else if (recvd == 0) {
+    puts("Connection closed");
+    return WOLFSSL_CBIO_ERR_CONN_CLOSE;
+  }
 
-    printf("%s", "recv:");
-    for (int i = 0; i < sz; i ++) printf(" %02x", (unsigned char)buff[i]);
-    puts("");
+  printf("%s", "recv:");
+  for (int i = 0; i < sz; i++) printf(" %02x", (unsigned char)buff[i]);
+  puts("");
 
-    /* successful receive */
-    printf("received %d bytes from %i\n\n", sz, sockfd);
-    return recvd;
+  /* successful receive */
+  printf("received %d bytes from %i\n\n", sz, sockfd);
+  return recvd;
 }
 
-int my_IOSend(WOLFSSL* ssl, char* buff, int sz, void* ctx)
-{
-    /* By default, ctx will be a pointer to the file descriptor to write to.
-     * This can be changed by calling wolfSSL_SetIOWriteCtx(). */
-    int sockfd = *(int*)ctx;
-    int sent;
+int my_IOSend(WOLFSSL* ssl, char* buff, int sz, void* ctx) {
+  /* By default, ctx will be a pointer to the file descriptor to write to.
+   * This can be changed by calling wolfSSL_SetIOWriteCtx(). */
+  int sockfd = *(int*)ctx;
+  int sent;
 
-    printf("%s", "send:");
-    for (int i = 0; i < sz; i ++) printf(" %02x", (unsigned char)buff[i]);
-    puts("");
+  printf("%s", "send:");
+  for (int i = 0; i < sz; i++) printf(" %02x", (unsigned char)buff[i]);
+  puts("");
 
-    /* Receive message from socket */
-    if ((sent = send(sockfd, buff, sz, 0)) == -1) {
-        /* error encountered. Be responsible and report it in wolfSSL terms */
+  /* Receive message from socket */
+  if ((sent = send(sockfd, buff, sz, 0)) == -1) {
+    /* error encountered. Be responsible and report it in wolfSSL terms */
 
-        fprintf(stderr, "IO SEND ERROR: ");
-        switch (errno) {
-        #if EAGAIN != EWOULDBLOCK
-        case EAGAIN: /* EAGAIN == EWOULDBLOCK on some systems, but not others */
-        #endif
-        case EWOULDBLOCK:
-            fprintf(stderr, "would block\n");
-            return WOLFSSL_CBIO_ERR_WANT_WRITE;
-        case ECONNRESET:
-            fprintf(stderr, "connection reset\n");
-            return WOLFSSL_CBIO_ERR_CONN_RST;
-        case EINTR:
-            fprintf(stderr, "socket interrupted\n");
-            return WOLFSSL_CBIO_ERR_ISR;
-        case EPIPE:
-            fprintf(stderr, "socket EPIPE\n");
-            return WOLFSSL_CBIO_ERR_CONN_CLOSE;
-        default:
-            fprintf(stderr, "general error\n");
-            return WOLFSSL_CBIO_ERR_GENERAL;
-        }
+    fprintf(stderr, "IO SEND ERROR: ");
+    switch (errno) {
+#if EAGAIN != EWOULDBLOCK
+      case EAGAIN: /* EAGAIN == EWOULDBLOCK on some systems, but not others */
+#endif
+      case EWOULDBLOCK:
+        fprintf(stderr, "would block\n");
+        return WOLFSSL_CBIO_ERR_WANT_WRITE;
+      case ECONNRESET:
+        fprintf(stderr, "connection reset\n");
+        return WOLFSSL_CBIO_ERR_CONN_RST;
+      case EINTR:
+        fprintf(stderr, "socket interrupted\n");
+        return WOLFSSL_CBIO_ERR_ISR;
+      case EPIPE:
+        fprintf(stderr, "socket EPIPE\n");
+        return WOLFSSL_CBIO_ERR_CONN_CLOSE;
+      default:
+        fprintf(stderr, "general error\n");
+        return WOLFSSL_CBIO_ERR_GENERAL;
     }
-    else if (sent == 0) {
-        puts("Connection closed");
-        return 0;
-    }
+  } else if (sent == 0) {
+    puts("Connection closed");
+    return 0;
+  }
 
-    /* successful send */
-    printf("sent %d bytes to %i\n\n", sz, sockfd);
-    return sent;
+  /* successful send */
+  printf("sent %d bytes to %i\n\n", sz, sockfd);
+  return sent;
 }
 
-int main(int argc, char** argv)
-{
-    int                ret; 
-    int                sockfd = SOCKET_INVALID;
-    struct sockaddr_in servAddr;
-    char               buff[256];
-    size_t             len;
+int main(int argc, char** argv) {
+  int ret;
+  int sockfd = SOCKET_INVALID;
+  struct sockaddr_in servAddr;
+  char buff[256];
+  size_t len;
 
-    /* declare wolfSSL objects */
-    WOLFSSL_CTX* ctx = NULL;
-    WOLFSSL*     ssl = NULL;
+  /* declare wolfSSL objects */
+  WOLFSSL_CTX* ctx = NULL;
+  WOLFSSL* ssl = NULL;
 
-    /* Check for proper calling convention */
-    if (argc != 4) {
-        printf("usage: %s <IPv4 address> <port> <path (e.g. /)>\n", argv[0]);
-        return 0;
+  /* Check for proper calling convention */
+  if (argc != 4) {
+    printf("usage: %s <IPv4 address> <port> <path (e.g. /)>\n", argv[0]);
+    return 0;
+  }
+
+  char* tlsHost = argv[1];
+  char* tlsPort = argv[2];
+  char* reqPath = argv[3];
+
+  /* Initialize wolfSSL */
+  wolfSSL_Init();
+
+  /* Create a socket that uses an internet IPv4 address,
+   * Sets the socket to be stream based (TCP),
+   * 0 means choose the default protocol. */
+  if ((sockfd = socket(AF_INET, SOCK_STREAM, 0)) == -1) {
+    fprintf(stderr, "ERROR: failed to create the socket\n");
+    ret = -1;
+    goto exit;
+  }
+
+  /* Create and initialize WOLFSSL_CTX */
+  if ((ctx = wolfSSL_CTX_new(wolfTLS_client_method())) == NULL) {
+    fprintf(stderr, "ERROR: failed to create WOLFSSL_CTX\n");
+    ret = -1;
+    goto exit;
+  }
+
+  /* Load client certificates into WOLFSSL_CTX */
+  if ((ret = wolfSSL_CTX_load_verify_buffer(
+           ctx, rootCert, sizeof(rootCert) - 1 /* omit terminal null */,
+           WOLFSSL_FILETYPE_PEM)) != WOLFSSL_SUCCESS) {
+    fprintf(stderr, "ERROR: failed to load cert, please check the buffer.\n");
+    goto exit;
+  }
+
+  /* Register callbacks */
+  wolfSSL_SetIORecv(ctx, my_IORecv);
+  wolfSSL_SetIOSend(ctx, my_IOSend);
+
+  /* Initialize the server address struct with zeros */
+  memset(&servAddr, 0, sizeof(servAddr));
+
+  /* NEW: look up name */
+  struct hostent* hostnm = gethostbyname(tlsHost);
+  if (hostnm == NULL) {
+    puts("ERROR: gethostbyname() failed");
+    goto exit;
+  }
+
+  /* Fill in the server address */
+  servAddr.sin_family = AF_INET; /* using IPv4 */
+  servAddr.sin_port = htons(atoi(tlsPort));
+  servAddr.sin_addr.s_addr = *((unsigned long*)hostnm->h_addr);
+
+  /* Open TCP connection to the server */
+  if (connect(sockfd, (struct sockaddr*)&servAddr, sizeof(servAddr)) == -1) {
+    fprintf(stderr, "ERROR: failed to connect\n");
+    ret = -1;
+    goto exit;
+  }
+
+  /* Create a WOLFSSL object */
+  if ((ssl = wolfSSL_new(ctx)) == NULL) {
+    fprintf(stderr, "ERROR: failed to create WOLFSSL object\n");
+    ret = -1;
+    goto exit;
+  }
+
+  /* NEW: enable SNI */
+  if ((ret = wolfSSL_UseSNI(ssl, WOLFSSL_SNI_HOST_NAME, tlsHost,
+                            strlen(tlsHost))) != WOLFSSL_SUCCESS) {
+    fprintf(stderr, "ERROR: failed to set host for SNI\n");
+    goto exit;
+  }
+
+  /* Attach wolfSSL to the socket */
+  wolfSSL_set_fd(ssl, sockfd);
+
+  /* Turn on domain name check */
+  if ((ret = wolfSSL_check_domain_name(ssl, tlsHost)) != WOLFSSL_SUCCESS) {
+    puts("Failed to enable domain name check");
+    goto exit;
+  };
+
+  /* TLS handshake with server */
+  if ((ret = wolfSSL_connect(ssl)) != WOLFSSL_SUCCESS) {
+    int err = wolfSSL_get_error(ssl, ret);
+    fprintf(stderr, "ERROR: failed to connect to wolfSSL, error %i\n", err);
+    goto exit;
+  }
+
+  char getReqBuff[1024];
+  snprintf(getReqBuff, sizeof(getReqBuff),
+           "GET %s HTTP/1.0\r\nHost: %s\r\n\r\n", reqPath, tlsHost);
+  len = strlen(getReqBuff);
+
+  /* Send the message to the server */
+  if ((ret = wolfSSL_write(ssl, getReqBuff, len)) != len) {
+    fprintf(stderr, "ERROR: failed to write\n");
+    goto exit;
+  }
+
+  do {
+    ret = wolfSSL_read(ssl, buff,
+                       sizeof(buff) - 1 /* leave space for null-termination */);
+    if (ret == -1) {
+      fprintf(stderr, "ERROR: failed to read\n");
+      goto exit;
+    }
+    if (ret > 0) {
+      buff[ret] = 0;  // null-terminate the string
+      printf("data: %s\n", buff);
     }
 
-    char *tlsHost = argv[1];
-    char *tlsPort = argv[2];
-    char *reqPath = argv[3];
+  } while (ret > 0);
 
-    /* Initialize wolfSSL */
-    wolfSSL_Init();
-
-    /* Create a socket that uses an internet IPv4 address,
-     * Sets the socket to be stream based (TCP),
-     * 0 means choose the default protocol. */
-    if ((sockfd = socket(AF_INET, SOCK_STREAM, 0)) == -1) {
-        fprintf(stderr, "ERROR: failed to create the socket\n");
-        ret = -1; 
-        goto exit;
-    }
-
-    /* Create and initialize WOLFSSL_CTX */
-    if ((ctx = wolfSSL_CTX_new(wolfTLS_client_method())) == NULL) {
-        fprintf(stderr, "ERROR: failed to create WOLFSSL_CTX\n");
-        ret = -1;
-        goto exit;
-    }
-
-    /* Load client certificates into WOLFSSL_CTX */
-    if ((ret = wolfSSL_CTX_load_verify_buffer(ctx, rootCert, sizeof(rootCert) - 1 /* omit terminal null */, WOLFSSL_FILETYPE_PEM))
-        != WOLFSSL_SUCCESS) {
-        fprintf(stderr, "ERROR: failed to load cert, please check the buffer.\n");
-        goto exit;
-    }
-
-    /* Register callbacks */
-    wolfSSL_SetIORecv(ctx, my_IORecv);
-    wolfSSL_SetIOSend(ctx, my_IOSend);
-
-    /* Initialize the server address struct with zeros */
-    memset(&servAddr, 0, sizeof(servAddr));
-
-    /* NEW: look up name */
-    struct hostent *hostnm = gethostbyname(tlsHost);
-    if (hostnm == NULL) {
-       puts("ERROR: gethostbyname() failed");
-       goto exit;
-    }
-
-    /* Fill in the server address */
-    servAddr.sin_family = AF_INET;  /* using IPv4 */
-    servAddr.sin_port = htons(atoi(tlsPort));
-    servAddr.sin_addr.s_addr = *((unsigned long *)hostnm->h_addr);
-
-    /* Open TCP connection to the server */
-    if (connect(sockfd, (struct sockaddr*) &servAddr, sizeof(servAddr)) == -1) {
-        fprintf(stderr, "ERROR: failed to connect\n");
-        ret = -1; 
-        goto exit;
-    }
-
-    /* Create a WOLFSSL object */
-    if ((ssl = wolfSSL_new(ctx)) == NULL) {
-        fprintf(stderr, "ERROR: failed to create WOLFSSL object\n");
-        ret = -1; 
-        goto exit;
-    }
-
-    /* NEW: enable SNI */
-    if ((ret = wolfSSL_UseSNI(ssl, WOLFSSL_SNI_HOST_NAME, tlsHost, strlen(tlsHost))) != WOLFSSL_SUCCESS) {
-        fprintf(stderr, "ERROR: failed to set host for SNI\n");
-        goto exit;
-    }
-
-    /* Attach wolfSSL to the socket */
-    wolfSSL_set_fd(ssl, sockfd);
-
-    /* Turn on domain name check */
-    if ((ret = wolfSSL_check_domain_name(ssl, tlsHost)) != WOLFSSL_SUCCESS) {
-        puts("Failed to enable domain name check");
-        goto exit;
-    };
-
-    /* TLS handshake with server */
-    if ((ret = wolfSSL_connect(ssl)) != WOLFSSL_SUCCESS) {
-        int err = wolfSSL_get_error(ssl, ret);
-        fprintf(stderr, "ERROR: failed to connect to wolfSSL, error %i\n", err);
-        goto exit;
-    }
-
-    char getReqBuff[1024];
-    snprintf(getReqBuff, sizeof(getReqBuff), "GET %s HTTP/1.0\r\nHost: %s\r\n\r\n", reqPath, tlsHost);
-    len = strlen(getReqBuff);
-
-    /* Send the message to the server */
-    if ((ret = wolfSSL_write(ssl, getReqBuff, len)) != len) {
-        fprintf(stderr, "ERROR: failed to write\n");
-        goto exit;
-    }
-
-    do {
-        ret = wolfSSL_read(ssl, buff, sizeof(buff) - 1 /* leave space for null-termination */);
-        if (ret == -1) {
-            fprintf(stderr, "ERROR: failed to read\n");
-            goto exit;
-        }
-        if (ret > 0) {
-            buff[ret] = 0;  // null-terminate the string
-            printf("data: %s\n", buff);
-        }
-
-    } while (ret > 0);
-
-    ret = 0;
+  ret = 0;
 
 exit:
-    /* Cleanup and return */
-    if (ssl)
-        wolfSSL_free(ssl);      /* Free the wolfSSL object              */
-    if (sockfd != SOCKET_INVALID)
-        close(sockfd);           /* Close the connection to the server   */
-    if (ctx)
-        wolfSSL_CTX_free(ctx);  /* Free the wolfSSL context object          */
-    wolfSSL_Cleanup();          /* Cleanup the wolfSSL environment          */
+  /* Cleanup and return */
+  if (ssl) wolfSSL_free(ssl); /* Free the wolfSSL object              */
+  if (sockfd != SOCKET_INVALID)
+    close(sockfd);                /* Close the connection to the server   */
+  if (ctx) wolfSSL_CTX_free(ctx); /* Free the wolfSSL context object          */
+  wolfSSL_Cleanup();              /* Cleanup the wolfSSL environment          */
 
-    return ret;               /* Return reporting a success               */
+  return ret; /* Return reporting a success               */
 }
--- jstlsclient.c
+++ jstlsclient.c
@@ -1,67 +1,62 @@
 
-#include <stdlib.h>
-#include <stdio.h>
-#include <string.h>
+#include <emscripten.h>
 #include <errno.h>
-
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 #include <wolfssl/options.h>
 #include <wolfssl/ssl.h>
 
 #include "certs/cacert-isgrootx1.c"
+// #include "certs/cacerts.c"
 
 int ret;
 size_t len;
-char buff[1600]; // originally 256
+char buff[8192];  // originally 256
 
 WOLFSSL_CTX *ctx = NULL;
 WOLFSSL *ssl = NULL;
+int inited = 0;
 
-int (*jsProvideEncryptedFromNetwork)(char *buff, int maxSize);
-void (*jsReceiveDecryptedFromLibrary)(char *buff, int size);
-int (*jsWriteEncryptedToNetwork)(char *buff, int size);
-// + JS expects to call write and recv here as appropriate
+EM_ASYNC_JS(int, jsProvideEncryptedFromNetwork, (char *buff, int sz), {
+  const bytesRead = await Module.provideEncryptedFromNetwork(buff, sz);
+  return bytesRead;
+});
 
-void init(
-    int (*newJsProvideEncryptedFromNetwork)(char *buff, int maxSize),
-    void (*newJsReceiveDecryptedFromLibrary)(char *buff, int size),
-    int (*newJsWriteEncryptedToNetwork)(char *buff, int size))
-{
-  wolfSSL_Init();
-  jsProvideEncryptedFromNetwork = newJsProvideEncryptedFromNetwork;
-  jsReceiveDecryptedFromLibrary = newJsReceiveDecryptedFromLibrary;
-  jsWriteEncryptedToNetwork = newJsWriteEncryptedToNetwork;
-}
+EM_JS(int, jsWriteEncryptedToNetwork, (char *buff, int sz), {
+  const bytesWritten = Module.writeEncryptedToNetwork(buff, sz);
+  return bytesWritten;
+});
 
-int my_IORecv(WOLFSSL *ssl, char *buff, int sz, void *ctx)
-{
+EM_JS(int, jsReceiveDecryptedFromLibrary, (char *buff, int sz), {
+  void 0;  // for code formatting!
+  Module.jsReceiveDecryptedFromLibrary(buff, sz);
+});
+
+int my_IORecv(WOLFSSL *ssl, char *buff, int sz, void *ctx) {
   int recvd = jsProvideEncryptedFromNetwork(buff, sz);
 
-  if (recvd == -1)
-  {
+  if (recvd == -1) {
     fprintf(stderr, "General error\n");
     return WOLFSSL_CBIO_ERR_GENERAL;
   }
 
-  if (recvd == 0)
-  {
-    puts("Connection closed");
+  if (recvd == 0) {
+    puts("0 bytes received, connection closed");
     return WOLFSSL_CBIO_ERR_CONN_CLOSE;
   }
 
   printf("%s", "recv:");
-  for (int i = 0; i < sz; i++)
-    printf(" %02x", (unsigned char)buff[i]);
+  for (int i = 0; i < sz; i++) printf(" %02x", (unsigned char)buff[i]);
   puts("");
 
   printf("received %d bytes from JS\n\n", recvd);
   return recvd;
 }
 
-int my_IOSend(WOLFSSL *ssl, char *buff, int sz, void *ctx)
-{
+int my_IOSend(WOLFSSL *ssl, char *buff, int sz, void *ctx) {
   printf("%s", "send:");
-  for (int i = 0; i < sz; i++)
-    printf(" %02x", (unsigned char)buff[i]);
+  for (int i = 0; i < sz; i++) printf(" %02x", (unsigned char)buff[i]);
   puts("");
 
   int sent = jsWriteEncryptedToNetwork(buff, sz);
@@ -70,74 +65,81 @@ int my_IOSend(WOLFSSL *ssl, char *buff, int sz, void *ctx)
   return sent;
 }
 
-void cleanup()
-{
-  if (ssl)
-  {
+void cleanup() {
+  if (ssl) {
     wolfSSL_free(ssl);
     ssl = NULL;
   }
-  if (ctx)
-  {
+  if (ctx) {
     wolfSSL_CTX_free(ctx);
     ctx = NULL;
   }
   wolfSSL_Cleanup();
 }
 
-int handshake(char *tlsHost, char *tlsPort)
-{
+int handshake(char *tlsHost) {
+  if (!inited) {
+    puts("handshake: init");
+    wolfSSL_Init();
+    inited = 1;
+  }
+
+  puts("handshake: create context");
   ctx = wolfSSL_CTX_new(wolfTLS_client_method());
-  if (ctx == NULL)
-  {
+  if (ctx == NULL) {
     fprintf(stderr, "ERROR: failed to create WOLFSSL_CTX\n");
     ret = -1;
     goto exit;
   }
 
-  ret = wolfSSL_CTX_load_verify_buffer(ctx, rootCert, sizeof(rootCert) - 1 /* omit terminal null */, WOLFSSL_FILETYPE_PEM);
-  if (ret != WOLFSSL_SUCCESS)
-  {
+  puts("handshake: load certs");
+  ret = wolfSSL_CTX_load_verify_buffer(
+      ctx, rootCert, sizeof(rootCert) - 1 /* omit terminal null */,
+      WOLFSSL_FILETYPE_PEM);
+  if (ret != WOLFSSL_SUCCESS) {
     fprintf(stderr, "ERROR: failed to load cert, please check the buffer.\n");
     goto exit;
   }
 
+  // printf("handshake: certs consist of %li bytes\n", strlen(rootCert));
+
+  puts("handshake: set IO handlers");
   wolfSSL_SetIORecv(ctx, my_IORecv);
   wolfSSL_SetIOSend(ctx, my_IOSend);
 
+  puts("handshake: create SSL obj");
   ssl = wolfSSL_new(ctx);
-  if (ssl == NULL)
-  {
+  if (ssl == NULL) {
     fprintf(stderr, "ERROR: failed to create WOLFSSL object\n");
     ret = -1;
     goto exit;
   }
 
+  puts("handshake: set SNI");
   ret = wolfSSL_UseSNI(ssl, WOLFSSL_SNI_HOST_NAME, tlsHost, strlen(tlsHost));
-  if (ret != WOLFSSL_SUCCESS)
-  {
+  if (ret != WOLFSSL_SUCCESS) {
     fprintf(stderr, "ERROR: failed to set host for SNI\n");
     goto exit;
   }
 
+  puts("handshake: set domain check");
   ret = wolfSSL_check_domain_name(ssl, tlsHost);
-  if (ret != WOLFSSL_SUCCESS)
-  {
+  if (ret != WOLFSSL_SUCCESS) {
     puts("Failed to enable domain name check");
     goto exit;
   };
 
+  puts("handshake: connect");
   ret = wolfSSL_connect(ssl);
-  if (ret != WOLFSSL_SUCCESS)
-  {
+  if (ret != WOLFSSL_SUCCESS) {
     int err = wolfSSL_get_error(ssl, ret);
     fprintf(stderr, "ERROR: failed to connect to wolfSSL, error %i\n", err);
     goto exit;
   }
 
   // char getReqBuff[1024];
-  // snprintf(getReqBuff, sizeof(getReqBuff), "GET %s HTTP/1.0\r\nHost: %s\r\n\r\n", reqPath, tlsHost);
-  // len = strlen(getReqBuff);
+  // snprintf(getReqBuff, sizeof(getReqBuff), "GET %s HTTP/1.0\r\nHost:
+  // %s\r\n\r\n", reqPath, tlsHost); len = strlen(getReqBuff);
 
   // if ((ret = wolfSSL_write(ssl, getReqBuff, len)) != len) {
   //     fprintf(stderr, "ERROR: failed to write\n");
@@ -145,8 +147,8 @@ int handshake(char *tlsHost, char *tlsPort)
   // }
 
   // do {
-  //     ret = wolfSSL_read(ssl, buff, sizeof(buff) - 1 /* leave space for null-termination */);
-  //     if (ret == -1) {
+  //     ret = wolfSSL_read(ssl, buff, sizeof(buff) - 1 /* leave space for
+  //     null-termination */); if (ret == -1) {
   //         fprintf(stderr, "ERROR: failed to read\n");
   //         goto exit;
   //     }
@@ -157,6 +159,7 @@ int handshake(char *tlsHost, char *tlsPort)
 
   // } while (ret > 0);
 
+  puts("handshake: done");
   return 0;
 
 exit:
@@ -164,32 +167,24 @@ exit:
   return -1;
 }
 
-int receiveData()
-{ // prompt WolfSSL to read (via JS callback) and decrypt data
-  ret = wolfSSL_read(ssl, buff, sizeof(buff) - 1 /* space to null-terminate string */);
-  if (ret < 0)
-  {
+int readData(char *buff, int sz) {
+  ret = wolfSSL_read(ssl, buff, sz);
+  if (ret < 0) {
     fprintf(stderr, "ERROR: failed to read\n");
     return ret;
   }
-  if (ret == 0)
-  {
+  if (ret == 0) {
     fprintf(stderr, "Zero-length read\n");
     return ret;
   }
 
-  buff[ret] = 0; // null-terminate the string for printing
-  printf("data: %s\n", buff);
-
-  jsReceiveDecryptedFromLibrary(buff, ret);
   return ret;
 }
 
-void writeData(char *buff, int sz)
-{ // ask WolfSSL to encrypt an send (via JS callback) data
+int writeData(char *buff, int sz) {
   ret = wolfSSL_write(ssl, buff, sz);
-  if (ret != sz)
-  {
+  if (ret != sz) {
     fprintf(stderr, "ERROR: failed to write\n");
   }
+  return ret;
 }
